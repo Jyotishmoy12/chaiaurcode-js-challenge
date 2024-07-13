7412e4ba8bfd85ca6fccc3ae76ee721d17df8d9f
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-07-13 12:58:26 +0530

    variables and datatypes

---
+++ DAY1/script.js
@@ -0,0 +1,50 @@
+//Declare a variable using Var and assign it to a number and log the value in the console.
+
+var number=1234
+console.log(number)
+
+//Declare a variable using let and assign it to a number
+
+let num=12345
+console.log(num)
+
+//Declare a variable using const and assign it to a boolean
+
+const bool=false
+console.log(bool)
+
+
+//create variables of different data types and log each variables types
+// using the typeof operator
+
+let number2=12345
+console.log(typeof(number2))
+
+let string="hello"
+console.log(typeof(string))
+
+let boo=true
+console.log(typeof(boolean)) // undefind
+
+let obj={
+    name:"sai",
+    age:20
+}
+
+console.log(typeof(obj)) // object
+
+let array=[1, "hello"]
+console.log(typeof(array)) //object
+
+let value=12345
+value=23456
+console.log(value)
+
+
+const val=12456
+val=234555
+console.log(val) //script.js:45 Uncaught TypeError: Assignment to constant variable.
+
+
+
+
