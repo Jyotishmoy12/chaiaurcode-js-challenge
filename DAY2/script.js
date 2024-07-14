e8ba22f10532a3e3d3f0ec769ef749d7a07fe37a
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-07-14 18:38:47 +0530

    day2

---
+++ DAY2/script.js
@@ -0,0 +1,58 @@
+console.log(2+3); // add
+
+console.log(3-2);
+
+console.log(3*2);
+
+console.log(3/2); // divide
+
+console.log(3%2);
+
+//Activity two
+
+let num=3;
+num+=3;
+console.log(num)
+
+
+// let num2=3;
+// num2-=3;
+// console.log(num2)
+
+
+//activity three
+
+let number1=3;
+let number2=4;
+if(number1>number2){
+    console.log(number1)
+}
+
+let numx=45;
+let numy=68;
+
+console.log(numx==numy)
+
+let nump=67;
+let numq=67;
+console.log(nump===numq)
+
+
+//Certainly! Here's a small example using numbers to demonstrate the && operator in JavaScript:
+
+// Define two numbers
+let num1 = 5;
+let num2 = 10;
+
+// Log the result code
+console.log((num1 > 0) && (num2>20));
+
+console.log((num1<10) || (num2<5))
+
+console.log((num1 > 0) && !(num2>20));
+
+// Example using ternary operator
+let val= 10;
+let result = (val > 0) ? "Positive" : "Non-positive";
+
+console.log(result); // Output: Positive
